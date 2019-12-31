4c406e931e552e8d73a8f735bd495d549e755fec
Daniel Watkins <danielmwatkins@gmail.com>
2019-12-31 13:23:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 """Master script to walk through the data analysis."""
+from lts.utilities import *
+from lts.computation import *
+
+#### Parameters ####
+data_save_loc = '~/data/'
+comp_save_loc = '~/computed/'
+image_save_loc = '~/images/'
+
+variables = ['ICEFRAC', 'LANDFRAC', 'TREFHT', 'T850', 'CLDLOW', 'CLDTOT', 'TGCLDLWP']
 
 #### Grouping Data ####
+get_CESMLE_data(variables, saveloc=data_save_loc)
+get_CESM2_data(variables, saveloc=data_save_loc)
+get_ERAI_data(variables, saveloc=data_save_loc)
+get_ERA5_data(variables, saveloc=data_save_loc)
 
 #### Computations ####
+compute_time_series(variables, dataloc=data_save_loc, saveloc=comp_save_loc)
+compute_ensemble_variability(variables, dataloc=data_save_loc, saveloc=comp_save_loc)
+compute_interannual_variability(variables, dataloc=data_save_loc, saveloc=comp_save_loc)
 
 #### Visualizations ####
-
+plot_annual_cycle(['LTS', 'TREFHT', 'T850', 'CLDLOW', 'CLDLOW'], dataloc=data_save_loc, saveloc=image_save_loc)
+plot_correlation(x='ICEFRAC', y='LTS', dataloc=data_save_loc, saveloc=image_save_loc)
+plot_correlation(x='ICEFRAC', y='TREFHT', dataloc=data_save_loc, saveloc=image_save_loc)
+plot_correlation(x='ICEFRAC', y='T850', dataloc=data_save_loc, saveloc=image_save_loc)
+plot_taylor(variables, dataloc=data_save_loc, saveloc=image_save_loc)
+                 
+map_annual_cycle(['LTS', 'TREFHT', 'T850', 'CLDLOW', 'CLDLOW'], dataloc=data_save_loc, saveloc=image_save_loc)
+map_ensemble_variability(['LTS', 'TREFHT', 'T850', 'CLDLOW', 'CLDLOW'], dataloc=comp_save_loc, saveloc=image_save_loc)
+map_interannual_variability(['LTS', 'TREFHT', 'T850', 'CLDLOW', 'CLDLOW'], dataloc=comp_save_loc, saveloc=image_save_loc)
